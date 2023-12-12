2057fe28c5ea927d3e4dd14a9a8a6439e9a6ac0e
tudeptrai <tu416164@gmail.com>
2023-12-12 16:59:03 +0700

    payment stripe

--- be/server.js
+++ be/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('./connection');
-const router = require('./routes');
-
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
@@ -16,7 +14,24 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/", router);
+app.use("/", require('./routes'));
+
+app.post('/create-payment', async (req, res) => {
+  const { amount } = req.body;
+  console.log(amount);
+  // const a = amount;
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: 'vnd',
+      payment_method_types: ['card']
+    });
+    res.status(200).json(paymentIntent)
+  } catch (e) {
+    console.log(e.message);
+    res.status(400).json(e.message);
+  }
+})
 
 server.listen(8080, () => {
   console.log('server running at port', 8080)
