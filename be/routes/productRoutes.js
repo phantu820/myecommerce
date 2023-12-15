349bd18812673aed52b6ba28ef4b576a258dcb42
tudeptrai <tu416164@gmail.com>
2023-12-15 12:52:48 +0700

    update code

--- be/routes/productRoutes.js
+++ be/routes/productRoutes.js
@@ -5,10 +5,12 @@ router.get("/", productController.getAllProduct);
 router.get("/:id", productController.getProductById);
 router.post("/", productController.createProduct);
 router.patch("/:id", productController.updateProduct);
-router.delete("/", productController.deleteProduct);
+router.delete("/:id", productController.deleteProduct);
 router.get("/category/:categoryId", productController.filterByCategory);
 router.post("/add-to-cart", productController.addtoCart);
 router.post("/increase-cart", productController.increaseCart);
 router.post("/decrease-cart", productController.decreaseCart);
 router.post("/remove-from-cart", productController.removefromCart);
+
 module.exports = router;
+
--- be/routes/userRoutes.js
+++ be/routes/userRoutes.js
@@ -6,6 +6,6 @@ router.post('/signup', userController.signup);
 router.post('/login', userController.login);
 router.get('/', userController.getUsers);
 router.get("/:id/orders", userController.getUserOrders);
-router.post("/:id/updateNotifications", userController.updateNotifications);
+router.post("/:id/updateNotifications", userController.updateUserNotifications);
 
 module.exports = router;
